f7bcea6bf1bbdb3a87ebf9a611345de9e16d0eb4
wangwenyuan <827287829@qq.com>
2022-12-11 22:47:42 +0800

    修改

--- database.php
+++ database.php
@@ -1,3 +1,7 @@
 <?php
 require_once './phptodo/PHPTODO.php';
-require_once '../malltodo-php.database.config/database.config.create.php';
+if (file_exists('../malltodo-php.database.config/database.config.create.php')) {
+    require_once '../malltodo-php.database.config/database.config.create.php';
+} else {
+    TDREDIRECT(TDUU("Index/Index/index", array(), "index.php"));
+}

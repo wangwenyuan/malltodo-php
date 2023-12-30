1eade63de20211e0b836bd51fbf13f643bab3efe
wangwenyuan <827287829@qq.com>
2023-12-30 22:59:57 +0800

    修改

--- phptodo/lib/tools/TDPAGE.php
+++ phptodo/lib/tools/TDPAGE.php
@@ -24,11 +24,16 @@ class TDPAGE
     // 总页数
     public $allPageNum = 0;
 
-    public function __construct($total, $size)
+    public function __construct($total, $size, $param = array())
     {
         $this->total = $total;
         $this->listRows = $size;
-        $this->p = (int) $_GET["p"];
+        if(count($param) > 0){
+            $this->p = (int) $param["p"];
+        }else{
+            $this->p = (int) $_GET["p"];
+        }
+        
         if ($this->p == 0 || $this->p == 1) {
             $this->p = 1;
         }
@@ -36,17 +41,20 @@ class TDPAGE
         $this->allPageNum = ceil((string) $total / (string) $this->listRows);
     }
 
-    public function show()
+    public function show($param = array())
     {
+        if(count($param) > 0){
+            $arr = $param;
+        }else{
+            $arr = $_GET;
+        }
         $html = "<span>共" . $this->allPageNum . "页，每页" . $this->listRows . "条，当前第" . $this->p . "页</span>";
         if ($this->p == 1) {
-            $arr = $_GET;
             $arr["p"] = ($this->p) + 1;
             $html = $html . "<span>首页</span><span>上一页</span><a href='" . TDU($arr) . "' >下一页</a>";
             $arr["p"] = $this->allPageNum;
             $html = $html . "<a href='" . TDU($arr) . "' >尾页</a>";
         } else if ($this->p == $this->allPageNum) {
-            $arr = $_GET;
             $arr["p"] = 1;
             $html = $html . "<a href='" . TDU($arr) . "'>首页</a>";
             $arr["p"] = $this->p - 1;
@@ -56,7 +64,6 @@ class TDPAGE
         } else if ($this->p > $this->allPageNum) {
             $html = "";
         } else {
-            $arr = $_GET;
             $arr["p"] = 1;
             $html = $html . "<a href='" . TDU($arr) . "' >首页</a>";
             $arr["p"] = $this->p - 1;
@@ -67,7 +74,6 @@ class TDPAGE
             $html = $html . "<a href='" . TDU($arr) . "' >尾页</a>";
         }
         $gotopage = "<span>转到：<select onchange=\"self.location.href=this.options[this.selectedIndex].value\">";
-        $arr = $_GET;
         for ($i = 0; $i < $this->allPageNum; $i = $i + 1) {
             $arr["p"] = ($i + 1);
             $selected = "";
